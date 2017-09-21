76e327d3d302c06c9c1b7ded04491cea963af7e3
andreasschacht <schacht@kaliber5.de>
2017-09-21 21:55:49 +0200

    resolve an empty array to true on validation

--- Serializer/JsonApiSerializationVisitor.php
+++ Serializer/JsonApiSerializationVisitor.php
@@ -114,7 +114,7 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
      */
     protected function validateJsonApiDocument($data)
     {
-        if ((is_array($data) || $data instanceof \Traversable) && count($data) > 0 && $this->hasResource($data)) {
+        if ((is_array($data) || $data instanceof \Traversable) && ((count($data) > 0 && $this->hasResource($data)) || count($data) === 0)) {
             return true;
         }
 
