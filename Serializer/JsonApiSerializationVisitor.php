407c6fe3f4cf2633ab883a98ffb1ed0c67c7cce4
Sergey Chernecov <sergey.chernecov@intexsys.lv>
2018-01-15 17:19:10 +0200

    Error handling based on Validation constraints has been added

--- Serializer/JsonApiSerializationVisitor.php
+++ Serializer/JsonApiSerializationVisitor.php
@@ -11,8 +11,6 @@
 
 namespace Mango\Bundle\JsonApiBundle\Serializer;
 
-use Hateoas\Representation\CollectionRepresentation;
-use Hateoas\Representation\PaginatedRepresentation;
 use JMS\Serializer\Accessor\AccessorStrategyInterface;
 use JMS\Serializer\Context;
 use JMS\Serializer\JsonSerializationVisitor;
@@ -21,6 +19,7 @@ use JMS\Serializer\Metadata\ClassMetadata;
 use JMS\Serializer\Naming\PropertyNamingStrategyInterface;
 use Mango\Bundle\JsonApiBundle\EventListener\Serializer\JsonEventSubscriber;
 use Metadata\MetadataFactoryInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * @author Steffen Brem <steffenbrem@gmail.com>
@@ -37,6 +36,9 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
      */
     protected $showVersionInfo;
 
+    /**
+     * @var bool
+     */
     protected $isJsonApiDocument = false;
 
     /**
@@ -96,11 +98,25 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
         return $root;
     }
 
+    /**
+     * Build json api root
+     *
+     * @param mixed      $data
+     * @param array|null $meta
+     *
+     * @return array
+     */
     protected function buildJsonApiRoot($data, array $meta = null)
     {
-        $root = array(
-            'data' => $data,
-        );
+        if ($data instanceof ConstraintViolationListInterface) {
+            $root = [
+                'errors' => $data,
+            ];
+        } else {
+            $root = [
+                'data' => $data,
+            ];
+        }
 
         if ($meta) {
             $root['meta'] = $meta;
@@ -133,6 +149,10 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
             return true;
         }
 
+        if ($data instanceof ConstraintViolationListInterface) {
+            return true;
+        }
+
         return $this->isResource($data);
     }
 
@@ -147,16 +167,12 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
 
         $root = $this->getRoot();
 
-        // TODO: Error handling
-        if (isset($root['data']) && array_key_exists('errors', $root['data'])) {
-            return parent::getResult();
-        }
-
         if ($root) {
             $data = array();
             $meta = array();
             $included = array();
             $links = array();
+            $errors = array();
 
             if (array_key_exists('data', $root)) {
                 $data = $root['data'];
@@ -172,6 +188,10 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
 
             if (isset($root['links'])) {
                 $links = $root['links'];
+            }
+
+            if (isset($root['errors'])) {
+                $errors = $root['errors'];
             }
 
             if (!is_null($data)) {
@@ -205,10 +225,13 @@ class JsonApiSerializationVisitor extends JsonSerializationVisitor
                 $root['links'] = $links;
             }
 
-            $root['data'] = $data;
-
-            if ($included) {
-                $root['included'] = array_values($included);
+            if (is_array($errors)) {
+                $root['errors'] = $errors;
+            } else {
+                $root['data'] = $data;
+                if ($included) {
+                    $root['included'] = array_values($included);
+                }
             }
 
             $this->setRoot($root);
